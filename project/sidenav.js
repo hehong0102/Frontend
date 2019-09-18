d022dd72a253ad2f0451a6dd594f7690382d4f8f
hehong0102 <54666914+hehong0102@users.noreply.github.com>
2019-09-18 09:51:09 +0200

    Add files via upload

---
+++ project/sidenav.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "170px";
+  document.getElementById("history-main").style.marginLeft = "180px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("history-main").style.marginLeft= "10px";
+}
